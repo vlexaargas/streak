9c2c076282def2e94f0d4738619694cf2a184147
Mark Nguyen <pnguyen1097@gmail.com>
2014-03-01 20:25:33 -0500

    Setup Gulp and Karma.

---
+++ Gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    karma = require('gulp-karma');
+
+var paths = {
+  scripts: "./app/js/**/*.js",
+  styles: "./app/scss/**/*.scss",
+  tests: "./tests/**/*Spec.js"
+}
+
+process.env['CHROME_CANARY_BIN'] = 'google-chrome-unstable';
+
+gulp.task('sass', function () {
+  gulp.src("./app/scss/main.scss")
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('uglify', function() {
+  gulp.src(paths.scripts)
+    .pipe(concat('production.js'))
+    .pipe(uglify({outSourceMap: false}))
+    .pipe(gulp.dest('./app/dist'))
+});
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src(paths.tests)
+    .pipe(karma({
+      configFile: './config/karma.conf.js',
+      action: 'run'
+    }));
+});
+
+gulp.task('default', function() {
+
+
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['uglify']);
+  gulp.watch(paths.styles, ['sass']);
+
+  gulp.src(paths.tests)
+    .pipe(karma({
+      configFile: './config/karma.conf.js',
+      action: 'watch'
+    }));
+});
