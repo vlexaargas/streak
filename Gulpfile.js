fa34575ae623d5b375fe847e50a1c4d8f89046d9
Alex Vargas <vargas.84@osu.edu>
2014-03-08 16:55:15 -0500

    Setup local server and livereload.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,37 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    karma = require('gulp-karma');
+    karma = require('gulp-karma'),
+    refresh = require('gulp-livereload'),
+    lrserver = require('tiny-lr')(),
+    express = require('express'),
+    livereload = require('connect-livereload'),
+    livereloadport = 35729,
+    serverport = 5000;
+
+var server = express();
+server.use(livereload({
+  port: livereloadport
+}));
+
+server.use(express.static('./app'));
 
 var paths = {
   scripts: "./app/js/**/*.js",
   styles: "./app/scss/**/*.scss",
-  tests: "./tests/**/*Spec.js"
+  tests: "./tests/**/*Spec.js",
+  html: "./app/**/*.html"
 };
+
+gulp.task('html', function() {
+  gulp.src(paths.html)
+    .pipe(refresh(lrserver));
+});
+
+gulp.task('serve', function() {
+  server.listen(serverport);
+  lrserver.listen(livereloadport);
+});
 
 gulp.task('sass', function () {
   gulp.src("./app/scss/main.scss")
@@ -33,17 +57,20 @@ gulp.task('test', function() {
 });
 
 gulp.task('default', function() {
-
-
+  gulp.run('serve');
+  gulp.run('watch');
 });
 
 gulp.task('watch', function () {
 //  gulp.watch(paths.scripts, ['uglify']);
   gulp.watch(paths.styles, ['sass']);
+  gulp.watch(paths.html, ['html']);
 
   gulp.src(paths.tests)
     .pipe(karma({
       configFile: './config/karma.conf.js',
       action: 'watch'
     }));
+
+
 });
